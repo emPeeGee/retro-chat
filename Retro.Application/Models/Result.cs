82052fb3979eee4a14db37aba1de353e856b61fd
emPeeGee <eempeegee@gmail.com>
2025-06-05 20:36:40 +0300

    refactor: make Result generic

--- Retro.Application/Models/Result.cs
+++ Retro.Application/Models/Result.cs
@@ -1,8 +1,10 @@
-public class Result
+public class Result<T>
 {
   public bool IsSuccess { get; init; }
   public string? Error { get; init; }
+  public T? Value { get; init; }
 
-  public static Result Success() => new() { IsSuccess = true };
-  public static Result Failure(string error) => new() { IsSuccess = false, Error = error };
+  public static Result<T> Success(T value) => new() { IsSuccess = true, Value = value };
+  public static Result<T> Failure(string error) => new() { IsSuccess = false, Error = error };
 }
+
